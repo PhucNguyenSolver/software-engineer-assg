cc6cffcf2786898c1fe785d0d74857c11bca47f0
toan lam <lamtoan557@gmail.com>
2021-11-02 02:00:56 +0700

    Done filter

--- frontend/src/components/FilterBar.js
+++ frontend/src/components/FilterBar.js
@@ -1,18 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react";
+import React, { useState } from "react";
+import JSONDATA from './MOCK_DATA.json';
 
 
 function FilterBar() {
+  const [data, setData] = useState(JSONDATA)
+  var [filterNameInit,setFilterNameInit] = useState('No filter here...')
+
+  function FilterFunction(){
+    filterNameInit = setFilterNameInit('Sort by increasing price')
+    setData(data.slice().sort((a,b) => a.price - b.price))
+  }
+
+
   return (
     <div class="dropdown" style={{position:'absolute', right:'0px'}}>
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"
-  style={{backgroundColor: '#F0A12A'}}>
-    No filter here...
+  style={{backgroundColor: '#F0A12A',width:'200px'}}>
+    {filterNameInit}
   </button>
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><button type="button" class="btn btn-outline-primary dropdown-item" onClick={FilterFunction}>Sort by increasing price</button></li>
+    <li><button type="button" class="btn btn-outline-primary dropdown-item">Sort by decreasing price</button></li>
+    <li><button type="button" class="btn btn-outline-primary dropdown-item">No filter here...</button></li>
   </ul>
 </div>
   );
--- frontend/src/components/TaskSearch.js
+++ frontend/src/components/TaskSearch.js
@@ -1,34 +1,71 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-import MenuInGen from './MenuInGen.js'
+// import MenuInGen from './MenuInGen.js'
 import RenderElement from './RenderElement.js'
-import FilterBar from './FilterBar.js'
+// import FilterBar from './FilterBar.js'
 import JSONDATA from './MOCK_DATA.json'
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
-
-
-function Filter(props) {
-  if (props.searchTerm === "") {
-    return <MenuInGen />
-  }
-  return JSONDATA.filter((val)=>{
-    if (val.food_name.toLowerCase().includes(props.searchTerm.toLowerCase())){
-        return val
-    }
-    }).map((val,key) => {
-    return <div key ={key} style={{display:'inline-block'}}>
-    <RenderElement val={val}/>
-    </div>  
-    })
-  }
+import FoodTypeList from './FoodTypeList.js'
+import FoodInMenu from './FoodInMenu.js'
 
 
 
-
-function TaskSearch() {
+function TaskSearch() { 
 
   const [searchTerm,setSearchTerm] = useState('')
+  const [data, setData] = useState(JSONDATA)
+  var [filterNameInit,setFilterNameInit] = useState('No filter here...')
+
+  function MenuInGen() {
+    return (
+        <div >
+        <div style={{width:'100%'}}>
+            <FoodTypeList />
+        </div>
+        <div style={{backgroundColor:'#F0F8FF',height:'750px',width:'100%'}}>
+            {
+              data.map((val) => {
+              return <FoodInMenu name={val.food_name} price={val.price} image={val.img} />
+            })
+            }
+        </div>
+        </div>
+    );
+  }
+
+  function FilterBar() {
+  
+    function FilterFunction(){
+      setFilterNameInit("Sort by increasing price")
+      setData(data.slice().sort((a,b) => a.price - b.price))
+      
+    }
+    
+    function FilterFunctionDesc(){
+      setFilterNameInit('Sort by decreasing price')
+      setData(data.slice().sort((a,b) => -a.price + b.price))
+    }
+  
+    function NoFilterFunction(){
+      setFilterNameInit('No filter here...')
+      setData(JSONDATA)
+    }
+
+    return (
+      <div class="dropdown" style={{position:'absolute', right:'0px'}}>
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"
+    style={{backgroundColor: '#F0A12A',width:'200px'}}>
+      {filterNameInit}
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <li><button type="button" class="btn btn-outline-primary dropdown-item" onClick={FilterFunction}>Sort by increasing price</button></li>
+      <li><button type="button" class="btn btn-outline-primary dropdown-item" onClick={FilterFunctionDesc}>Sort by decreasing price</button></li>
+      <li><button type="button" class="btn btn-outline-primary dropdown-item" onClick={NoFilterFunction}>No filter here...</button></li>
+    </ul>
+  </div>
+    );
+  }
 
   function HandleSearch() {
     var filtered;
