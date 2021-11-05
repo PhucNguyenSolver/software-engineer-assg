93438d8982242cfda75a7b57c03175f683d8612c
triyys <ngoductri123@gmail.com>
2021-11-05 15:59:16 +0700

    initial HandleOrder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import CartInfo from './components/cartinfo/CartInfo';
 import { FoodInfo } from './components/pages/FoodInfo/FoodInfo';
+import HandleOrder from './components/checkout/HandleOrder';
 
 function App() {
     return (
         <div className="App">
-			<CartInfo
+			{/* <CartInfo
 				cartId="#012345"
 				cartHeading="Gà rán 4 miếng"
 				imgUrl="https://jollibee.com.vn/uploads/dish/427e7a3136f84a-4mingggin.png"
-			></CartInfo>
+			></CartInfo> */}
+			<HandleOrder/>
         </div>
     );
 }
