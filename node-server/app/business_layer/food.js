8c02f986c7dd7328563d3d93932e12ca71bfa1c6
Nguyen Phuc Vinh <nguyenphucvinh05072001@gmail.com>
2021-11-13 12:41:06 +0700

    change architect

--- node-server/app/business_layer/food.js
+++ node-server/app/business_layer/food.js
@@ -2,7 +2,7 @@ const db = require('../data_layer')
 
 const getFoodById = async function(req, res) {
     const foodId = req.params.id
-    const food = await db.Foods.find({}).exec();
+    const food = await db.Foods.findById(foodId).exec();
     res.status(200).send(food);
 }
 
